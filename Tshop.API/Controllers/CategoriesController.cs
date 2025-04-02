4c9685fb126c93ae2bfd2753180cfad2858d95ab
MohamadGhnemat <aohamad.ahmad9696@gmail.com>
2025-04-02 21:26:40 +0200

    Add project files.

---
+++ Tshop.API/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tshop.API.Data;
+using Tshop.API.Models;
+
+namespace Tshop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult getAll()
+        {
+           var categories= _context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getById([FromRoute]int id)
+        {
+            var category = _context.Categories.Find(id);
+            return category == null ? NotFound() : Ok(category);
+        }
+
+        [HttpPost("")]
+
+        public IActionResult Create([FromBody] Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(getById), new { category.Id }, category);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete([FromRoute]int id) { 
+        var category = _context.Categories.Find(id);
+            if(category == null) return NotFound();
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return NoContent();//204
+        }
+    }
+}
---
+++ Tshop.API/Program.cs
@@ -0,0 +1,60 @@
+
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using Tshop.API.Data;
+
+
+namespace Tshop.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen();
+
+            
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+                app.MapScalarApiReference();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            //var context = new ApplicationDbContext();
+
+            //try
+            //{
+            //    context.Database.CanConnect();
+            //    Console.WriteLine("done");
+
+            //}
+            //catch (Exception ex) {
+            //    Console.WriteLine("error");
+            //}
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
